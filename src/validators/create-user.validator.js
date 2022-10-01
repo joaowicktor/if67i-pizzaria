71c5329b8570af39d717d5bd36e4026272e9cb74
João Wicktor <joaowicktor@gmail.com>
2022-10-01 14:28:06 -0300

    translate createUserValidation messages

--- src/validators/create-user.validator.js
+++ src/validators/create-user.validator.js
@@ -6,5 +6,10 @@ export const createUserValidator = joi.object({
   password: joi
     .string()
     .pattern(/^[a-zA-Z0-9]{6,}$/)
-    .required(),
+    .required()
+    .messages({
+      'string.pattern.base': 'A senha deve conter no mínimo 6 caracteres e apenas letras e números',
+      'string.empty': 'A senha não pode ser vazia',
+      'any.required': 'A senha é obrigatória',
+    }),
 });
