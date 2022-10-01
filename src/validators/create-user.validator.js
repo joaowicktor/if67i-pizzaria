2b478b7000adade82d30983b868072a7a39866ed
João Wicktor <joaowicktor@gmail.com>
2022-10-01 14:30:06 -0300

    translate other fields from createUserValidator

--- src/validators/create-user.validator.js
+++ src/validators/create-user.validator.js
@@ -1,8 +1,15 @@
 import joi from 'joi';
 
 export const createUserValidator = joi.object({
-  name: joi.string().required(),
-  email: joi.string().email().required(),
+  name: joi.string().required().messages({
+    'string.empty': 'O nome não pode ser vazio',
+    'any.required': 'O nome é obrigatório',
+  }),
+  email: joi.string().email().required().messages({
+    'string.email': 'O e-mail deve ser válido',
+    'string.empty': 'O e-mail não pode ser vazio',
+    'any.required': 'O e-mail é obrigatório',
+  }),
   password: joi
     .string()
     .pattern(/^[a-zA-Z0-9]{6,}$/)
