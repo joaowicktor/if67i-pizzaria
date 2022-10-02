919ac20eb98e7f9762965887c55e1afed9fa84c4
João Wicktor <joaowicktor@gmail.com>
2022-10-02 12:34:35 -0300

    changed create post validation logic to multipart form data

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -6,6 +6,7 @@ import validator from '../validators/index.js';
 import { createPostValidator } from '../validators/create-post.validator.js';
 import { listPostFilterValidator } from '../validators/list-post-filter.validator.js';
 import { upload } from '../middlewares/file-upload.middleware.js';
+import dataParserMiddleware from '../middlewares/data-parser.middleware.js';
 
 const router = express.Router();
 
@@ -15,6 +16,9 @@ router.post(
   ac.auth(Permission.CREATE_POST),
   upload().single('image'),
   validator.file(),
+  dataParserMiddleware.parseMultipartFormDataToObject,
+  validator.body(createPostValidator),
   postsController.create
+);
 
 export default router;
--- src/validators/create-post.validator.js
+++ src/validators/create-post.validator.js
@@ -1,24 +1,33 @@
 import joi from 'joi';
 
 export const createPostValidator = joi.object({
-  name: joi.string().required().messages({
-    'string.empty': 'O nome não pode ser vazio',
-    'any.required': 'O nome é obrigatório',
-  }),
-  description: joi.string().required().messages({
-    'string.empty': 'A descrição não pode ser vazia',
-    'any.required': 'A descrição é obrigatória',
-  }),
-  ingredients: joi.array().min(1).items(joi.string()).required().messages({
-    'array.base': 'Os ingredientes devem ser uma lista',
-    'array.min': 'Deve haver pelo menos um ingrediente',
-    'any.required': 'Os ingredientes são obrigatórios',
-    'string.empty': 'Os ingredientes não podem ser vazios',
-  }),
-  price: joi.string().regex(new RegExp('^[0-9]+$')).required().messages({
-    'string.base': 'O preço deve ser uma string',
-    'string.empty': 'O preço não pode ser vazio',
-    'string.pattern.base': 'O preço deve ser em centavos',
-    'any.required': 'O preço é obrigatório',
-  }),
+  data: joi
+    .object({
+      name: joi.string().required().messages({
+        'string.empty': 'O nome não pode ser vazio',
+        'any.required': 'O nome é obrigatório',
+      }),
+      description: joi.string().required().messages({
+        'string.empty': 'A descrição não pode ser vazia',
+        'any.required': 'A descrição é obrigatória',
+      }),
+      ingredients: joi.array().min(1).items(joi.string()).required().messages({
+        'array.base': 'Os ingredientes devem ser uma lista',
+        'array.min': 'Deve haver pelo menos um ingrediente',
+        'any.required': 'Os ingredientes são obrigatórios',
+        'string.empty': 'Os ingredientes não podem ser vazios',
+      }),
+      price: joi.string().regex(new RegExp('^[0-9]+$')).required().messages({
+        'string.base': 'O preço deve ser uma string',
+        'string.empty': 'O preço não pode ser vazio',
+        'string.pattern.base': 'O preço deve ser em centavos',
+        'any.required': 'O preço é obrigatório',
+      }),
+    })
+    .required()
+    .messages({
+      'object.base': 'O corpo da requisição deve ser um objeto',
+      'object.empty': 'O corpo da requisição não pode ser vazio',
+      'any.required': 'O corpo da requisição é obrigatório',
+    }),
 });
