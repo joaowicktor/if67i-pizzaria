087513164e7f44c4f3ebfa8bbcc475888fa6afa7
João Wicktor <joaowicktor@gmail.com>
2022-10-12 21:54:29 -0300

    added base orders routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from '../routes/auth.routes.js';
 import usersRoutes from '../routes/users.routes.js';
 import pizzasRoutes from '../routes/pizzas.routes.js';
 import rolesRoutes from '../routes/roles.routes.js';
+import ordersRoutes from '../routes/orders.routes.js';
 
 const routes = express.Router();
 
@@ -10,5 +11,6 @@ routes.use('/auth', authRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/pizzas', pizzasRoutes);
 routes.use('/roles', rolesRoutes);
+routes.use('/orders', ordersRoutes);
 
 export default routes;
